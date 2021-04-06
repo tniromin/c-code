a77fe3174fe397eab468458eb4c31b300e61fb1d
tniromin <tniromin2@gmail.com>
2021-04-06 15:36:56 +0530

    Create cat in the hat.c

---
+++ cat in the hat.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//tniromin
+//The calculation is done before taking the relevant input or inputs
+//solution :Take the input first
+
+int main(){
+int sum=100,num;
+
+sum=sum+num;
+scanf("%d",&num);
+
+
+return 0;
+}
+//I know this is a lame logical error but everything must have a start.
